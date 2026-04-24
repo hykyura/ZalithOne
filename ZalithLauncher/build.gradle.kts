f5883b509305ec47dd3811e7f4059ea74431e9df
由风 <youfeng11@outlook.com>
2026-04-24 18:54:34 +0800

    chore: 更新 targetSdk 至 35

--- ZalithLauncher/build.gradle.kts
+++ ZalithLauncher/build.gradle.kts
@@ -71,7 +71,7 @@ android {
         applicationId = zalithPackageName
         applicationIdSuffix = ".v2"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = launcherVersionCode
         versionName = launcherVersionName
         manifestPlaceholders["launcher_name"] = launcherAPPName
